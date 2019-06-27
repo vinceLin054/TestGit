e40e787b7dd3ac3b7dc2c10497ac3c9e30940c70
vinceLin054 <vinceLin054@163.com>
2019-06-27 08:36:02 +0800

    IDEA edit

--- src/main/java/lemon/git/Apple.java
+++ src/main/java/lemon/git/Apple.java
@@ -4,6 +4,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("left");
 		System.out.println("right");
-		System.out.println("idea");
+		System.out.println("idea");//edit IDEA
 	}
 }
